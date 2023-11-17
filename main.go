ed446e3a62dd0d91db771cda19e3d84f12b75c5f
mayejacob <mayejacob3@gmail.com>
2023-11-17 21:53:47 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %v\n", name)
+	fmt.Fprintf(w, "Address = %v\n", address)
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	http.Handle("/form", http.HandlerFunc(formHandler))
+	http.Handle("/hello", http.HandlerFunc(helloHandler))
+	// http.Handle("/hello", HelloHandler{})
+
+	fmt.Printf("Starting server at port 8080\n")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Simple Go Web Server
+
+This is a simple Go web server that demonstrates handling static files, a form submission, and a basic API endpoint.
+
+## Features
+
+- Serves static files from the "static" directory.
+- Handles a simple form submission at the "/form" endpoint.
+- Provides a basic API endpoint at "/hello" for GET requests.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Mayejacob/go-server.git
